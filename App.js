807a3f0f560c3d3963fe23c8fcc8fec99aba8d30
Thierry Ravelonjanahary <t.ravelonjanahary@gmail.com>
2023-04-18 01:53:01 +0200

    ✨ (Tailwind CSS) apply with dark theme

--- App.js
+++ App.js
@@ -1,23 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { useColorScheme } from "nativewind";
+import { Platform, Switch, Text, View } from "react-native";
 
 const App = () => {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+
   return (
-    <View style={styles.container}>
-      <Text>TailWind CSS in React Native</Text>
-      <Text>{`[ ${Platform.OS} ]`}</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 justify-center items-center bg-green-50 dark:bg-green-900">
+      <Text className="text-2xl dark:text-white">
+        TailWind CSS in React Native
+      </Text>
+      <View className="flex-row justify-center items-center">
+        <Text className="font-semibold dark:text-gray-50">
+          {`[${Platform.OS}]`} Dark theme{" "}
+        </Text>
+        <Switch
+          value={colorScheme === "dark"}
+          onChange={toggleColorScheme}
+          // thumbColor="#D5D8DC"
+          trackColor={{ true: "#ABEBC6" }}
+        />
+      </View>
+      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
     </View>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#E8F6F3",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
